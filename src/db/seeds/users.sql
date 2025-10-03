76dc920db677881e12d673b0d490bf887a578ff5
Alec Faustino <alecfaustino@outlook.com>
2025-10-03 15:27:03 -0600

    create users table and schema

---
+++ src/db/seeds/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, password_hash) VALUES
+('testuser1', 'testuser1@example.com', 'hashed_password1'),
+('testuser2', 'testuser2@example.com', 'hashed_password2'),
+('testuser3', 'testuser3@example.com', 'hashed_password3');
